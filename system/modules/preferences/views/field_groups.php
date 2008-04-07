4f353903d7913af947c924e982ebbeb75c8e9d5a
Adam Price <Adam Price@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2008-04-07 20:34:13 +0000

    Removed file/folder
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@59 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- system/modules/preferences/views/field_groups.php
+++
@@ -1,8 +0,0 @@
-<h2><?=$header?></h2>
-
-<ul id="preference_list">
-<?php
-    foreach($group as $key=>$value)
-        print "<li>" . anchor($form_link . '/' . $key,$value['name'],'class="icon_cog"') . "</li>";
-?>
-</ul>
