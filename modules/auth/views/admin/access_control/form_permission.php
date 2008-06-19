621f55ecb1165e65c15af684c9284dd463309ce6
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2008-06-19 17:34:45 +0000

    Added cellspacing option to the statistics table
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@111 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- modules/auth/views/admin/access_control/form_permission.php
+++ modules/auth/views/admin/access_control/form_permission.php
@@ -1,11 +1,11 @@
 <h2><?=$header?></h2>
 
 <?=form_open('auth/admin/acl_permissions/save')?>  
-<table width=100% cellspacing=0>
+<table width="100%" cellspacing="0">
     <tr>
-        <td width=33%><b><?=$this->lang->line('access_groups')?></b></td>
-        <td width=33%><b><?=$this->lang->line('access_resources')?></b></td>
-        <td width=33%><b><?=$this->lang->line('access_actions')?></b></td>
+        <td width="33%"><b><?=$this->lang->line('access_groups')?></b></td>
+        <td width="33%"><b><?=$this->lang->line('access_resources')?></b></td>
+        <td width="33%"><b><?=$this->lang->line('access_actions')?></b></td>
     </tr>
     <tr>
         <td><div class="scrollable_tree"><ul id="groups"><?=$this->access_control_model->buildGroupSelector(($_POST['id']!=NULL))?></ul></div></td>
@@ -14,8 +14,8 @@
     </tr>
     <tr>
         
-        <td colspan=3>
-            <b><?=$this->lang->line('access')?>:</b><br>
+        <td colspan="3">
+            <b><?=$this->lang->line('access')?>:</b><br/>
             <?=form_radio('allow','Y',$this->validation->set_radio('allow','Y')) . $this->lang->line('access_allow')?>
             <?=form_radio('allow','N',$this->validation->set_radio('allow','N')) . $this->lang->line('access_deny')?>
         </td>
