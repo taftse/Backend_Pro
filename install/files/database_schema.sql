7d2cc50ad926f82f5d68aaf45bb651e7dcbbd3fd
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2008-11-13 22:31:20 +0000

    This extra field has been added to the database install schema, Closes #30
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@158 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- install/files/database_schema.sql
+++ install/files/database_schema.sql
@@ -299,6 +299,7 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   `session_id` varchar(40) character set latin1 NOT NULL default '0',
   `ip_address` varchar(16) character set latin1 NOT NULL default '0',
   `user_agent` varchar(50) character set latin1 NOT NULL,
+  `user_data` text NOT NULL,
   `last_activity` int(10) unsigned NOT NULL default '0',
   PRIMARY KEY  (`session_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
