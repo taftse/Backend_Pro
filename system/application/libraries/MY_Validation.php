1ad282619c1c21f0622b1eacc309bc378cc27a3a
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2008-12-02 07:41:49 +0000

    Added an extra space to a WHERE clause in the MY_Validation library when checking if an email/username is spare. The problem seemed to be the MySQL query was thinking id!= was a column. Closes #58
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@167 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- system/application/libraries/MY_Validation.php
+++ system/application/libraries/MY_Validation.php
@@ -38,16 +38,16 @@
 
 			log_message('debug','MY_Validation Class Initialized');
 		}
-        
+
         /**
          * Set Default Value
-         * 
+         *
          * Assigns a default value to a form field
-         * 
+         *
          * @access public
          * @param mixed $data Field name OR Array
          * @param mixed $value Field value
-         * @return void 
+         * @return void
          */
         function set_default_value($data=NULL, $value=NULL)
         {
@@ -64,7 +64,7 @@
             $_POST[$data]   = $value;
             return;
         }
-        
+
 		/**
 		 * Output Validation Errors
 		 *
@@ -85,12 +85,12 @@
 			}
 			return;
 		}
-        
+
         /**
         * Check for valid captcha
-        * 
+        *
         * Contact the ReCaptcha server and check the input is valid
-        * 
+        *
         * @access public
         * @return boolean
         */
@@ -98,24 +98,24 @@
         {
             // Makre sure auth library is loaded
             $this->CI->load->module_library('auth','Userlib');
-            
+
             // Make sure the captcha library is loaded
             $this->CI->load->module_library('recaptcha','Recaptcha');
-            
+
             // Set the error message
             $this->CI->validation->set_message('valid_captcha', $this->CI->lang->line('userlib_validation_captcha'));
-            
+
             // Perform check
             $this->CI->recaptcha->recaptcha_check_answer($this->CI->input->server('REMOTE_ADDR'), $this->CI->input->post('recaptcha_challenge_field'), $this->CI->input->post('recaptcha_response_field'));
-            
+
             return $this->CI->recaptcha->is_valid;
         }
-        
+
         /**
         * Check that the username is spare
-        * 
+        *
         * Check that the username given is not in use
-        * 
+        *
         * @access public
         * @param string $username Username
         * @return boolean
@@ -124,20 +124,20 @@
         {
             // Makre sure auth library is loaded
             $this->CI->load->module_library('auth','Userlib');
-            
-            $query = $this->CI->user_model->fetch('Users',NULL,NULL,array('username'=>$username)); 
-            
+
+            $query = $this->CI->user_model->fetch('Users',NULL,NULL,array('username'=>$username));
+
             // Set the error message
             $this->CI->validation->set_message('spare_username', $this->CI->lang->line('userlib_validation_username'));
-        
-            return ($query->num_rows() == 0) ? TRUE : FALSE; 
+
+            return ($query->num_rows() == 0) ? TRUE : FALSE;
         }
-        
+
         /**
         * Check that the email is spare
-        * 
+        *
         * Check that the username given is not in use by another user
-        * 
+        *
         * @access public
         * @param string $email Email
         * @retrun boolean
@@ -145,21 +145,21 @@
         function spare_email($email)
         {
             // Makre sure auth library is loaded
-            $this->CI->load->module_library('auth','Userlib');  
-            
+            $this->CI->load->module_library('auth','Userlib');
+
             $query = $this->CI->user_model->fetch('Users',NULL,NULL,array('email'=>$email));
-            
+
             // Set the error message
             $this->CI->validation->set_message('spare_email', $this->CI->lang->line('userlib_validation_email'));
-            
+
             return ($query->num_rows() == 0) ? TRUE : FALSE;
         }
-        
+
         /**
         * Check Spare Username
-        * 
+        *
         * When modifying a user check the username is spare
-        * 
+        *
         * @access public
         * @param string $username Username
         * @return boolean
@@ -168,20 +168,20 @@
         {
             // Makre sure auth library is loaded
             $this->CI->load->module_library('auth','Userlib');
-            
-            $query = $this->CI->user_model->fetch('Users',NULL,NULL,array('username'=>$username,'id!='=>$this->CI->input->post('id'))); 
-            
+
+            $query = $this->CI->user_model->fetch('Users',NULL,NULL,array('username'=>$username,'id !='=>$this->CI->input->post('id')));
+
             // Set the error message
             $this->CI->validation->set_message('spare_edit_username', $this->CI->lang->line('userlib_validation_username'));
-        
-            return ($query->num_rows() == 0) ? TRUE : FALSE; 
+
+            return ($query->num_rows() == 0) ? TRUE : FALSE;
         }
-        
+
         /**
         * Check Spare Email
-        * 
+        *
         * When modifying a user check the email is spare
-        * 
+        *
         * @access public
         * @param string $email Email
         * @retrun boolean
@@ -189,13 +189,13 @@
         function spare_edit_email($email)
         {
             // Makre sure auth library is loaded
-            $this->CI->load->module_library('auth','Userlib');  
-            
-            $query = $this->CI->user_model->fetch('Users',NULL,NULL,array('email'=>$email,'id!='=>$this->CI->input->post('id')));
-            
+            $this->CI->load->module_library('auth','Userlib');
+
+            $query = $this->CI->user_model->fetch('Users',NULL,NULL,array('email'=>$email,'id !='=>$this->CI->input->post('id')));
+
             // Set the error message
             $this->CI->validation->set_message('spare_edit_email', $this->CI->lang->line('userlib_validation_email'));
-            
+
             return ($query->num_rows() == 0) ? TRUE : FALSE;
         }
 	}
