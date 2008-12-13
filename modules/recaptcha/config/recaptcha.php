538fc73804a2c2fe9937b53bcb7e070986102e94
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2008-12-13 18:36:48 +0000

    BackendPro 0.4 final commit
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@177 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- modules/recaptcha/config/recaptcha.php
+++ modules/recaptcha/config/recaptcha.php
@@ -1,10 +1,10 @@
 <?php
   $config['recaptcha'] = array(
-      'public'=>'6LceBQEAAAAAAIZzgIMuYdb7LS1NB7JQFL1gCs5z',
-      'private'=>'6LceBQEAAAAAAO2t9o35DVzviYC8ZUE3XHLD_1tI',
+      'public'=>'',
+      'private'=>'',
       'RECAPTCHA_API_SERVER' =>'http://api.recaptcha.net',
       'RECAPTCHA_API_SECURE_SERVER'=>'https://api-secure.recaptcha.net',
       'RECAPTCHA_VERIFY_SERVER' =>'api-verify.recaptcha.net',
       'theme' => 'white'
-); 
+);
 ?>
