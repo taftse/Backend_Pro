5dc3f27f0101a8261972035df5d5277ee78998b4
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2008-12-22 18:26:12 +0000

    Added a new statistic to the dashboard panel to show the CI version.
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@188 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- system/application/language/english/dashboard_lang.php
+++ system/application/language/english/dashboard_lang.php
@@ -1,32 +1,37 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
-	/**
-	 * Dashboard Language Array
-	 *
-	 * This file contains all language strings used by the administration 
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Dashboard Language Array
+ *
+ * This file contains all language strings used by the administration
 	 * area dashboard controller.
-	 * 
-	 * Used only by system/application/controllers/admin/home.php
-	 *
-	 * @package			BackendPro
-	 * @subpackage		Languages
-	 * @author			Adam Price
-	 * @copyright		Copyright (c) 2008
-	 * @license			http://www.gnu.org/licenses/lgpl.html
-	 */
-	
-	/* EXAMPLE widget */
-	$lang['dashboard_example'] = "Example Widget";
-	$lang['dashboard_example_body'] = "You can put what ever you want in these widgets, pictures, tables or plain text.";
+ *
+ * @package		BackendPro
+ * @subpackage 	Languages
+ * @author		Adam Price
+ * @copyright	Copyright (c) 2008, Adam Price
+ * @license		http://www.gnu.org/licenses/lgpl.html
+ * @link		http://www.kaydoo.co.uk/projects/backendpro
+ * @filesource
+ */
 
-	/* STATISTICS widget */
-	$lang['dashboard_statistics'] = "Statistics";
-	$lang['dashboard_statistics_system_status'] = "System Status";
-	$lang['dashboard_statistics_total_members'] = "Total Members";
-	$lang['dashboard_statistics_total_unactivated_members'] = "Total Unactivated Members";
-	$lang['dashboard_statistics_version'] = "Version";
-	$lang['dashboard_statistics_online'] = "Online";
-	$lang['dashboard_statistics_offline'] = "Offline";
-	$lang['dashboard_statistics_user_registration'] = "User Registration";
-	$lang['dashboard_statistics_name'] = "Name";
-	$lang['dashboard_statistics_value'] = "Value";
-?>
+// ------------------------------------------------------------------------
+
+/* EXAMPLE widget */
+$lang['dashboard_example'] = "Example Widget";
+$lang['dashboard_example_body'] = "You can put what ever you want in these widgets, pictures, tables or plain text.";
+
+/* STATISTICS widget */
+$lang['dashboard_statistics'] = "Statistics";
+$lang['dashboard_statistics_system_status'] = "System Status";
+$lang['dashboard_statistics_total_members'] = "Total Members";
+$lang['dashboard_statistics_total_unactivated_members'] = "Total Unactivated Members";
+$lang['dashboard_statistics_bep_version'] = "BeP Version";
+$lang['dashboard_statistics_ci_version'] = "CI Version";
+$lang['dashboard_statistics_online'] = "Online";
+$lang['dashboard_statistics_offline'] = "Offline";
+$lang['dashboard_statistics_user_registration'] = "User Registration";
+$lang['dashboard_statistics_name'] = "Name";
+$lang['dashboard_statistics_value'] = "Value";
+
+/* End of file dashboard_lang.php */
+/* Location: ./system/application/language/english/dashboard_lang.php */
--- system/application/views/admin/dashboard/statistics.php
+++ system/application/views/admin/dashboard/statistics.php
@@ -2,12 +2,13 @@
 	<thead>
 		<tr><th width="50%"><?=$this->lang->line('dashboard_statistics_name') ?></th><th><?=$this->lang->line('dashboard_statistics_value') ?></th></tr>
 	</thead>
-	
+
 	<tbody>
 		<tr><td><?=$this->lang->line('dashboard_statistics_system_status') ?></td><td><?=$system_status ?></td></tr>
 		<tr><td><?=$this->lang->line('dashboard_statistics_total_members') ?></td><td><?=$total_members ?></td></tr>
 		<tr><td><?=$this->lang->line('dashboard_statistics_total_unactivated_members') ?></td><td><?=$total_unactivated_members ?></td></tr>
 		<tr><td><?=$this->lang->line('dashboard_statistics_user_registration') ?></td><td><?=$user_registration ?></td></tr>
-		<tr><td><?=$this->lang->line('dashboard_statistics_version') ?></td><td><?=BEP_VERSION ?></td></tr>
+		<tr><td><?=$this->lang->line('dashboard_statistics_ci_version') ?></td><td><?=CI_VERSION ?></td></tr>
+		<tr><td><?=$this->lang->line('dashboard_statistics_bep_version') ?></td><td><?=BEP_VERSION ?></td></tr>
 	</tbody>
 </table>
