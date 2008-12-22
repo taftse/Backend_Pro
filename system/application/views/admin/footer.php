98efa41b8449e62eef83359d01c645b4b72c239f
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2008-12-22 18:35:02 +0000

    Fixed the location of the user guide inside the control panel. Closes #68
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@189 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- system/application/views/admin/footer.php
+++ system/application/views/admin/footer.php
@@ -3,8 +3,8 @@
             <a href="http://www.kaydoo.co.uk/projects/backendpro">BackendPro</a> &copy; Copyright 2008 - <a href="http://www.kaydoo.co.uk">Adam Price</a> -  All rights Reserved
         </div>
         <div id="version">
-            <a href="#top"><?=$this->lang->line('general_top')?></a> | 
-            <a href="../user_guide"><?=$this->lang->line('general_documentation')?></a> |  
+            <a href="#top"><?=$this->lang->line('general_top')?></a> |
+            <a href="<?=base_url()?>user_guide"><?=$this->lang->line('general_documentation')?></a> |
             Version <?=BEP_VERSION?></div>
     </div>
 </div>
