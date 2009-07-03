51ca7cae3270dd0b70deb1b34a323a1c9352fc4d
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2009-07-03 21:00:32 +0000

    Added the FlashStatus asset so it loads for the public front end. See #56
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@243 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- modules/site/config/bep_assets.php
+++ modules/site/config/bep_assets.php
@@ -87,7 +87,7 @@ $config['asset'][] = array('file'=>'bep_select_all.js', 'needs'=>'jquery');
  * Where <asset_n> is the name of the asset file specified in the array above.
  */
 $config['asset_group']['SITE'] = 'reset|typography';
-$config['asset_group']['PUBLIC'] = 'bep_front_layout';
+$config['asset_group']['PUBLIC'] = 'bep_front_layout|FlashStatus';
 $config['asset_group']['ADMIN'] = 'bep_admin_layout|bep_admin_style|FlashStatus|forms|buttons|bep_navigation|treeview|bep_icons|bep_select_all';
 
 $config['asset_group']['FORMS'] = "buttons|forms";
