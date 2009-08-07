f1ec03fc7468b12b4857f8f17816d68032dc2c4b
aprice30 <aprice30@eb1981d6-d9b1-d44f-b3a1-a14e2883e594>
2009-08-07 21:45:56 +0000

    Moved Admin_Controller and Public_Controller to there own seperate files. Keeping to 1 class per file. Closes #99
    git-svn-id: http://subversion.assembla.com/svn/backendpro/trunk@252 eb1981d6-d9b1-d44f-b3a1-a14e2883e594

--- system/application/libraries/MY_Controller.php
+++ system/application/libraries/MY_Controller.php
@@ -69,75 +69,8 @@ class Site_Controller extends Controller
 	}
 }
 
-/**
- * Public_Controller
- *
- * Extends the Site_Controller class so I can declare special Public controllers
- *
- * @package        BackendPro
- * @subpackage        Controllers
- */
-class Public_Controller extends Site_Controller
-{
-	function Public_Controller()
-	{
-		parent::Site_Controller();
+include_once("Admin_Controller.php");
+include_once("Public_Controller.php");
 
-		// Set container variable
-		$this->_container = $this->config->item('backendpro_template_public') . "container.php";
-
-		// Set public meta tags
-		//$this->bep_site->set_metatag('name','content',TRUE/FALSE);
-
-		// Load the PUBLIC asset group
-		$this->bep_assets->load_asset_group('PUBLIC');
-		
-		log_message('debug','BackendPro : Public_Controller class loaded');
-	}
-}
-
-/**
- * Admin_Controller
- *
- * Extends the Site_Controller class so I can declare special Admin controllers
- *
- * @package            BackendPro
- * @subpackage        Controllers
- */
-class Admin_Controller extends Site_Controller
-{
-	function Admin_Controller()
-	{
-		parent::Site_Controller();
-
-		// Set base crumb
-		$this->bep_site->set_crumb($this->lang->line('backendpro_control_panel'),'admin');
-
-		// Set container variable
-		$this->_container = $this->config->item('backendpro_template_admin') . "container.php";
-
-		// Set Pop container variable
-		$this->_popup_container = $this->config->item('backendpro_template_admin') . "popup.php";
-
-		// Make sure user is logged in
-		check('Control Panel');
-
-		// Check to see if the install path still exists
-		if( is_dir('install'))
-		{
-			flashMsg('warning',$this->lang->line('backendpro_remove_install'));
-		}
-
-		// Set private meta tags
-		//$this->bep_site->set_metatag('name','content',TRUE/FALSE);
-		$this->bep_site->set_metatag('robots','nofollow, noindex');
-		$this->bep_site->set_metatag('pragma','nocache',TRUE);
-
-		// Load the ADMIN asset group
-		$this->bep_assets->load_asset_group('ADMIN');
-
-		log_message('debug','BackendPro : Admin_Controller class loaded');
-	}
-}
 /* End of file MY_Controller.php */
 /* Location: ./system/application/libraries/MY_Controller.php */
